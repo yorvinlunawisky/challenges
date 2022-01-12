543391560523493d3dc1c3b57ed5636e6295610a
Yorvin Luna Wisky <yorvin0506@gmail.com>
2022-01-12 15:14:01 -0400

    Currency converter completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,51 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'converter';
+  //Property '...' has no initializer and is not definitely assigned in the constructor (I used an exclametion mark because of that. See below).
+
+  @ViewChild('f') currencyForm!: NgForm;
+  firstCurrency = 0;
+  secondCurrency = [56.57, 0.87, 1.14, 0.015 ];
+  result = 0;
+
+ 
+
+  // onSubmit(form: HTMLFormElement) {
+  //   console.log(form);
+  // }
+
+//   onSubmit(form: NgForm) {
+//     console.log(form);
+//   }
+
+    onSubmit() {
+      this.firstCurrency = this.currencyForm.value.amount;
+        if (this.firstCurrency > 0 && this.currencyForm.value.firstValue === 'dollar' && this.currencyForm.value.secondValue === 'peso') {
+          this.result =this.firstCurrency * 55.25
+        }  else if  (this.firstCurrency > 0 && this.currencyForm.value.firstValue === 'dollar' && this.currencyForm.value.secondValue === 'euro') {
+          this.result =this.firstCurrency * 0.87
+        }    
+           else if  (this.firstCurrency > 0 && this.currencyForm.value.firstValue === 'euro' && this.currencyForm.value.secondValue === 'peso') {
+          this.result =this.firstCurrency * 65.95
+        } 
+          else if  (this.firstCurrency > 0 && this.currencyForm.value.firstValue === 'euro' && this.currencyForm.value.secondValue === 'dollar') {
+          this.result =this.firstCurrency * 1.14
+        } else if  (this.firstCurrency > 0 && this.currencyForm.value.firstValue === 'peso' && this.currencyForm.value.secondValue === 'dollar') {
+          this.result = this.firstCurrency * 0.049
+        } else if  (this.firstCurrency > 0 && this.currencyForm.value.firstValue === 'peso' && this.currencyForm.value.secondValue === 'euro') {
+          this.result =this.firstCurrency * 0.017
+        }
+              //Same currencies
+          else if ((this.firstCurrency > 0 && this.currencyForm.value.firstValue === 'peso' && this.currencyForm.value.secondValue === 'peso')) {
+            this.result =this.firstCurrency * 1
+        } else if ((this.firstCurrency > 0 && this.currencyForm.value.firstValue === 'dollar' && this.currencyForm.value.secondValue === 'dollar')) {
+             this.result =this.firstCurrency * 1
+        } else if ((this.firstCurrency > 0 && this.currencyForm.value.firstValue === 'euro' && this.currencyForm.value.secondValue === 'euro')) {
+            this.result = this.firstCurrency * 1
+        }
+      
+    
+    }
 }
+
+
